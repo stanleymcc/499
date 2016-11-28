c9366534397fe417d258e32ef3c2fe47298dd3f7
MCplattipus94 <MCplattipus94@users.noreply.github.com>
2016-11-28 17:53:03 -0500

    client http update
    put request dosent follow through, need get request

--- client.py
+++ client.py
@@ -6,40 +6,56 @@
 # from user. If the connection is made the file sent will be sent.
 
 import socket
+import os
+import sys 
+import BaseHTTPServer
+import httplib
+import string
+def main():
 
-def main(filename):
-	# Create a new socket
-	s = socket.socket()
+	#s = socket.socket()
+	host = 'violet.cs.engr.uky.edu' #raw_input('Enter a host name: ')
+	port = int(input('Enter a port number: '))
 	
-	# Ask for user input of host name and port number to be used
-	host = str(raw_input('Enter a host name: '))
-	port = int(raw_input('Enter a port number: '))
+#	extension = '.doc'
+	#extension += raw_input('Enter filetype extension: ')
+
+#	for root, dirs, files in os.walk(os.getcwd()):
+#		for file in files:
+#			if file.endswith(extension):
+	proto = raw_input("choose command channel(raw/https)")	
 	
-	# If connection using host name and port number given is not made
-	# print error 'Connection to server failed.'
-	if s.connect_ex((host,port)) != 0:
-		s.close()
-		print('Connection to server failed.')
-		return False
 	
-	print('Connected to: ' + host)
-	
-	s.send(filename)
-	print('Sent filename: ' + filename)
-	
-	f = open(filename,'rb')
-	
-	# f.read(1024) sets the ability to read and write up to 1024 bytes on each 
-	# iteration of the loop.
-	l = f.read(1024)
-	while(l):
-		print('Sending...')
-		s.send(l)
+	if proto == 'raw':	
+		s = socket.socket()
+		s.connect((host, port))
+		s.recv(1024)
+		filename = './' + sys.argv[1]
+		print filename
+		s.send(filename)
+		f = open(filename,'rb')
 		l = f.read(1024)
-		
-	f.close()
-	print('Done sending ' + filename)
-	s.close()
-	print('Closed connection to: ' + host)
+		while(l):
+			print('Sending...')
+			s.send(l)
+			l = f.read(1024)
+			f.close()
+		print('Done sending ' + filename)
+		s.shutdown(2)
+		s.close()
 	
-	return True
+	#print('No more ' + extension + ' files found')
+		print('Closing connection to server')
+	elif proto == 'http':
+		s = httplib.HTTPConnection('violet.cs.engr.uky.edu',8000)
+		s.request("PUT",sys.argv[1])
+		response = s.getresponse()
+		print response.status, response.reason
+		s.close()
+#	s = socket.socket()
+#	s.connect((host, port))
+#	s.send('q')
+#	s.shutdown(2)
+#	s.close()
+
+main()
