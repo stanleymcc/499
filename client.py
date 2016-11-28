a99a1f1576986d6d8469f04b7ad65939492bc465
Lula Hogg <lulahogg@Lulas-MacBook-Air.local>
2016-11-28 14:04:39 -0500

    Adds comments to file

--- client.py
+++ client.py
@@ -1,17 +1,27 @@
+# Python 2.7
+# Authors: Coleman Platt, Erich Smith 
+# Last modified: Nov. 28, 2016
+
+# This program will ask for a host name and port number
+# from user. If the connection is made the file sent will be sent.
+
 import socket
 
 def main(filename):
-
+	# Create a new socket
 	s = socket.socket()
 	
+	# Ask for user input of host name and port number to be used
 	host = str(raw_input('Enter a host name: '))
 	port = int(raw_input('Enter a port number: '))
 	
+	# If connection using host name and port number given is not made
+	# print error 'Connection to server failed.'
 	if s.connect_ex((host,port)) != 0:
 		s.close()
 		print('Connection to server failed.')
 		return False
-		
+	
 	print('Connected to: ' + host)
 	
 	s.send(filename)
@@ -19,6 +29,8 @@ def main(filename):
 	
 	f = open(filename,'rb')
 	
+	# f.read(1024) sets the ability to read and write up to 1024 bytes on each 
+	# iteration of the loop.
 	l = f.read(1024)
 	while(l):
 		print('Sending...')
