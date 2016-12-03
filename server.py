d0270ba0db73e7ebf7e3580e93d63567cb81ae05
CaptJeanLuc1701 <erich.smith@att.net>
2016-12-02 23:39:41 -0500

    Update server.py
    New , clean version of server. Had to comment out the HTTPS stuff to get the raw socket part to work for testing. I'll be working from this version to add the Telnet stuff.
    Coleman - All I did was comment out the HTTPS stuff, I didn't modify it. If you need to work on those parts for testing you should just be able to uncomment those sections.

--- server.py
+++ server.py
@@ -1,87 +1,110 @@
 #Server
 # Python 2.7
-# Authors: Coleman Platt, Erich Smith 
-# Last modified: Nov. 28, 2016
- 
+# Authors: Coleman Platt, Erich Smith
+# Last modified: Dec. 2, 2016
+
+#This program creates servers with either raw socket/https/tcp
+#protocols. (needs to be added)
 
 import BaseHTTPServer
 import socket
+import sys
 
-def run_while_true(server_class=BaseHTTPServer.HTTPServer,
-      handler_class=BaseHTTPServer.BaseHTTPRequestHandler):
-      server_address = ('violet.cs.engr.uky.edu', 8000)
-      httpd = server_class(server_address, myHandler )
-      httpd.serve_forever()
-      
-      
+MAX_FILENAME = 255
 
-class myHandler(BaseHTTPRequestHandler):
+# #==============================Function that makes HTTPS server
+# def run_while_true(server_class=BaseHTTPServer.HTTPServer, handler_class=BaseHTTPServer.BaseHTTPRequestHandler):
+# #------------------------------sets host to violet and por 8000
+	  # server_address = ('violet.cs.engr.uky.edu', 8000)
+# #------------------------------makes server with server and handler attribus
+	  # httpd = server_class(server_address, myHandler )
+# #------------------------------loop through request handler until server interuptions
+	  # httpd.serve_forever()
 
-        #Handler for the PUT requests
-        def do_PUT(self):
-                print "----- SOMETHING WAS PUT!! ------"
-                print self.headers
-                length = int(self.headers['Content-Length'])
-                content = self.rfile.read(length)
-                self.send_response(200)
-                print content
+
+# #=============================Class for handler to refrence and handler
+# class myHandler(BaseHTTPRequestHandler):
+
+		# #Handler for the PUT requests
+		# def do_PUT(self):
+				# print "----- SOMETHING WAS PUT!! ------"
+				# print self.headers
+# #---------------------recives size of data in request
+				# length = int(self.headers['Content-Length'])
+# #---------------------recieves data
+				# content = self.rfile.read(length)
+# #---------------------sends report
+				# self.send_response(200)
+				# print content
 
 
 def main():
-    
-#    s = socket.socket()
-#    host = socket.gethostname()
-#    print host
-#    port = input('Enter a port number: ') 
-    
-#    s.bind((host,port))
-    
-#    s.listen(1)
-    run_while_true()
 
+	# Prompt user for which communication protocol to use.
+	proto = str(raw_input("Choose command channel: (raw/https/telnet) "))
 
- 
-    
-    while False:
-	c, addr = s.accept()
-        print('Got connection from',addr)
-        c.send('Connected')
+	# Raw socket connection.
+	if proto == "raw":
 
-	#while (geninput != close):
-	c.send('Enter a command or \'close\' to quit')
-	cmd = c.recv(1024)
-	if cmd == 'close':
-		c.shutdown(2)
-		c.close()
-		break
-	elif cmd == 'rec':
-		print "SERVER: Filename"
-		filename = c.recv(1024)
-	
-#	if filename == 'q':
-#			c.close()
-#			s.close()
-#			break
-		print ("CLIENT:", filename)
+		# Create socket and get host name (this machine).
+		s = socket.socket()
+		host = socket.gethostname()
 
-		recfile = open(filename,'wb')
-		infile =  c.recv(1024)
-		while (infile):
-			print "SERVER: Receiving..."
-			recfile.write(infile)
-			infile = c.recv(1024)
+		# Get port number from user.
+		port = int(input("Enter a port number: "))
 
-		print "SERVER: success?"
+		# Bind host and port number.
+		s.bind((host,port))
 
-		recfile.close()
-		c.shutdown(2)
-        	c.close()
+		# Listen for connections on this port number.
+		s.listen(1)
 
+		print("Listening on port " + str(port))
 
-		print "c close()"	
+		while True:
+			# Accept a connection request coming through the socket.
+			c, addr = s.accept()
+			print("Connected to ", addr)
 
+			# Get the name of the file from the client.
+			filename = c.recv(1024)
 
+			# Check if length of filename is too large.
+			if len(filename) > MAX_FILENAME:
+				print('Filename too big to save. Closing connection.')
+				s.close()
+				c.close()
+				return
+
+			# Open a binary file for writing, save as filename.
+			f = open(filename, 'wb')
+
+			# Get first batch of data.
+			l = c.recv(1024)
+
+			# While l != NULL, continue receiving data of the file.
+			while(l):
+				print('Receiving...' + filename)
+				f.write(l)
+				l = c.recv(1024)
+
+			# Print confirmation of file received and close all connections and file objects.
+			print('Received ' + filename + '.')
+			f.close()
+			c.close()
+			s.close()
+
+			print('Closed connection with ' + str(addr))
+			return
+
+	# HTTPS connection.
+	elif proto == "https":
+		print("HTTPS connection requested")
+		return
+
+	# Telnet connection.
+	elif proto == "telnet":
+		print("Telnet connnection requested")
+		return
 
 main()
-
-
